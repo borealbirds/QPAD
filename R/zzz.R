ccc1ad6b5de9a8c6a8c31d385ceff210802418b8
Peter Solymos <psolymos@gmail.com>
2015-05-14 10:15:51 -0600

    zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,14 @@
 .onAttach <- function(libname, pkgname){
     ver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname), 
                     fields=c("Version", "Date"))
-    packageStartupMessage(paste(pkgname, ver[1], "\t", ver[2]))
-    
+    packageStartupMessage(paste(pkgname, ver[1], "\t", ver[2],
+        "\nEstimates based on Solymos et al. 2013",
+        "\nUse 'load_BAM_QPAD()' to load estimates"))
     invisible(NULL)
 }
 
 .onUnload <- function(libpath){
+    if (exists(".BAMCOEFS", envir=.GlobalEnv))
+        rm(list=".BAMCOEFS", envir=.GlobalEnv)
     invisible(NULL)
 }
-
