f45079f5458b7e910bdc802a56efb94c34ec5b46
ecknight <ellyknight@gmail.com>
2023-01-04 14:28:05 -0700

    Add tag method option to bestmodel function

--- R/BAM_QPAD_functions.R
+++ R/BAM_QPAD_functions.R
@@ -184,29 +184,54 @@ function(species, model.sra, model.edr) {
 ## model ID for best supported model
 ## multi: best model randomly chosen based on model weights
 bestmodelBAMspecies <- 
-function(species, model.sra, model.edr, type=c("AIC", "BIC", "AICmulti", "BICmulti")) {
+function(species, model.sra, model.edr, type=c("AIC", "BIC", "AICmulti", "BICmulti"), tag=c(0,1)) {
     checkBAMestimates()
     type <- match.arg(type)
     x <- selectmodelBAMspecies(species, model.sra, model.edr)
-    if (type=="AIC") {
+    if(tag==1){
+      if (type=="AIC") {
         out <- list(sra=as.character(x$sra$model[which.min(x$sra$AIC)]),
-            edr=as.character(x$edr$model[which.min(x$edr$AIC)]))
-    }
-    if (type=="BIC") {
-            out <- list(sra=as.character(x$sra$model[which.min(x$sra$BIC)]),
-                edr=as.character(x$edr$model[which.min(x$edr$BIC)]))
-    }
-    if (type=="AICmulti") {
+                    edr=as.character(x$edr$model[which.min(x$edr$AIC)]))
+      }
+      if (type=="BIC") {
+        out <- list(sra=as.character(x$sra$model[which.min(x$sra$BIC)]),
+                    edr=as.character(x$edr$model[which.min(x$edr$BIC)]))
+      }
+      if (type=="AICmulti") {
         out <- list(sra=as.character(sample(x$sra$model, 1, 
-                prob=x$sra$wAIC)),
-            edr=as.character(sample(x$edr$model, 1, 
-                prob=x$edr$wAIC)))
-    }
-    if (type=="BICmulti") {
+                                            prob=x$sra$wAIC)),
+                    edr=as.character(sample(x$edr$model, 1, 
+                                            prob=x$edr$wAIC)))
+      }
+      if (type=="BICmulti") {
         out <- list(sra=as.character(sample(x$sra$model, 1, 
-                prob=x$sra$wBIC)),
-            edr=as.character(sample(x$edr$model, 1, 
-                prob=x$edr$wBIC)))
+                                            prob=x$sra$wBIC)),
+                    edr=as.character(sample(x$edr$model, 1, 
+                                            prob=x$edr$wBIC)))
+      }
+    }
+    
+    if(tag==0){
+      if (type=="AIC") {
+        out <- list(sra=as.character(x$sra$model[which.min(x$sra$AIC[1:15])]),
+                    edr=as.character(x$edr$model[which.min(x$edr$AIC)]))
+      }
+      if (type=="BIC") {
+        out <- list(sra=as.character(x$sra$model[which.min(x$sra$BIC[1:15])]),
+                    edr=as.character(x$edr$model[which.min(x$edr$BIC)]))
+      }
+      if (type=="AICmulti") {
+        out <- list(sra=as.character(sample(x$sra$model, 1, 
+                                            prob=x$sra$wAIC[1:15])),
+                    edr=as.character(sample(x$edr$model, 1, 
+                                            prob=x$edr$wAIC)))
+      }
+      if (type=="BICmulti") {
+        out <- list(sra=as.character(sample(x$sra$model, 1, 
+                                            prob=x$sra$wBIC[1:15])),
+                    edr=as.character(sample(x$edr$model, 1, 
+                                            prob=x$edr$wBIC)))
+      }
     }
     out
 }
